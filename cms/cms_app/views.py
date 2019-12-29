f49cb1e7835ac8ebf106815982a6c974b829ea15
heatherbaier <hmbaier@email.wm.edu>
2019-12-29 16:15:15 -0500

    fix menus

--- cms/cms_app/views.py
+++ cms/cms_app/views.py
@@ -1,4 +1,4 @@
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView, ListView
