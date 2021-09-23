9bce226b2932de9a608c419e8d2a59c432867307
Kerry Wang <zwang34@email.wm.edu>
2021-09-23 09:58:59 -0400

    Migrated to Django 3.2 autofields

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -136,3 +136,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Django 3.2 BigAutoFields
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
