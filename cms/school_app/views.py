edeb06793e5ef6738d6198b20c609a8aa20287ec
Kerry Wang <zwang34@wm.edu>
2022-02-22 10:47:06 -0500

    removed last button

--- cms/school_app/views.py
+++ cms/school_app/views.py
@@ -1,21 +1,27 @@
-from django.views.generic import TemplateView, ListView, DetailView
-from django_filters.views import FilterView
-from .models import School, SchoolResourcesFilter, Post
-from django.http import HttpResponse
 import csv
 
+from django.http import HttpResponse
+from django.views.generic import DetailView, ListView, TemplateView
+from django_filters.views import FilterView
+
+from .models import Post, School, SchoolResourcesFilter
+
 # Create your views here.
+
+
 class HomePageView(TemplateView):
     template_name = "index.html"
 
+
 class AboutView(TemplateView):
     template_name = "about.html"
+
 
 class SchoolListView(FilterView):
     model = School
     paginate_by = 20
     template_name = "schools_list.html"
-    ordering = ['school_name']
+    # ordering = ['school_name']
     filterset_class = SchoolResourcesFilter
 
     def get_context_data(self, **kwargs):
@@ -26,21 +32,26 @@ class SchoolListView(FilterView):
         context['get_copy'] = get_copy
         return context
 
+
 class SchoolProfileView(DetailView):
     model = School
     template_name = "school_profile.html"
 
+
 class MapView(TemplateView):
     template_name = "full_map.html"
+
 
 class PostList(ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     paginate_by = 3
     template_name = 'post_list.html'
 
+
 class PostDetail(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
 
 def SchoolListDownload(request):
     # Create the HttpResponse object with the appropriate CSV header.
