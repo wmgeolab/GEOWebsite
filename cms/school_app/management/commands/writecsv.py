29069c05601ba070ac6a84dfe498d328aa349a8c
Kerry Wang <kwzeyunwang@gmail.com>
2022-03-22 17:00:21 -0400

    write files to temp locations, then atomically replace

--- cms/school_app/management/commands/writecsv.py
+++ cms/school_app/management/commands/writecsv.py
@@ -17,7 +17,8 @@ class Command(BaseCommand):
         except FileExistsError:
             # Folder already exists
             pass
-        with open('csv/schools.csv', 'w', encoding='utf-8-sig') as f:
+        # Write new files next to the old ones, then atomically replace
+        with open('csv/schools.csv.tmp', 'w', encoding='utf-8-sig') as f:
             field_names = School._meta.fields
             field_names = [str(field).split('.')[-1] for field in field_names]
             writer = csv.DictWriter(f, field_names)
@@ -28,19 +29,23 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS(
             f'Wrote {count} records in {round(time.time()-now, 3)} seconds'))
         # Generate compressed versions
-        with open('csv/schools.csv', 'rb') as f:
+        with open('csv/schools.csv.tmp', 'rb') as f:
             data = f.read()
             # gzip
             self.stdout.write('Compressing with gzip...')
             now = time.time()
-            with gzip.open('csv/schools.csv.gz', 'wb') as g:
+            with gzip.open('csv/schools.csv.gz.tmp', 'wb') as g:
                 g.write(data)
             self.stdout.write(self.style.SUCCESS(
                 f'Finished in {round(time.time()-now, 3)} seconds'))
             # brotli
             self.stdout.write('Compressing with brotli...')
             now = time.time()
-            with open('csv/schools.csv.br', 'wb') as b:
+            with open('csv/schools.csv.br.tmp', 'wb') as b:
                 b.write(brotli.compress(data, mode=brotli.MODE_TEXT))
             self.stdout.write(self.style.SUCCESS(
                 f'Finished in {round(time.time()-now, 3)} seconds'))
+        # Replace
+        os.replace('csv/schools.csv.tmp', 'csv/schools.csv')
+        os.replace('csv/schools.csv.gz.tmp', 'csv/schools.csv.gz')
+        os.replace('csv/schools.csv.br.tmp', 'csv/schools.csv.br')
