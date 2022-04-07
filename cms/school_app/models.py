ec608436f7b71486838aeb2c89592599c974c9e7
Kerry Wang <kwzeyunwang@gmail.com>
2022-04-07 13:41:53 -0400

    create new models

--- cms/school_app/models.py
+++ cms/school_app/models.py
@@ -5,6 +5,53 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
+class SchoolV2(models.Model):
+    # primary key is implied
+    school_id = models.TextField()
+    country = models.TextField()
+    school_name = models.TextField()
+    sector = models.TextField(blank=True)
+    school_level = models.TextField(blank=True)
+    municipality = models.TextField(blank=True)
+    department = models.TextField(blank=True)
+    zone = models.TextField(blank=True)
+    address = models.TextField(blank=True)
+    lon = models.DecimalField(
+        max_digits=8,
+        decimal_places=5,
+        null=True,
+        validators=[MaxValueValidator(180), MinValueValidator(-180)],
+    )
+    lat = models.DecimalField(
+        max_digits=7,
+        decimal_places=5,
+        null=True,
+        validators=[MaxValueValidator(90), MinValueValidator(-90)],
+    )
+
+    def __str__(self) -> str:
+        return self.school_name
+
+    class Meta:
+        db_table = "schoolv2"
+
+
+class SchoolV2Session(models.Model):
+    # primary key is implied
+    school = models.ForeignKey(SchoolV2, models.CASCADE)
+    session = models.TextField(blank=True)
+    data_year = models.DecimalField(max_digits=4, decimal_places=0, null=True)
+    total_enrollment = models.DecimalField(max_digits=10, decimal_places=0, null=True)
+    test_score = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    gender_ratio = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.school}_{self.data_year}_{self.session}"
+
+    class Meta:
+        db_table = "schoolv2session"
+
+
 class School(models.Model):
     id = models.IntegerField(primary_key=True)
     country = models.TextField()
