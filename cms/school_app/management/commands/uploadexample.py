8d3b503b7cf5fb37869f343f4669920695a71e39
Devanshu Saxena <devthegreat974@gmail.com>
2022-04-27 10:59:35 -0400

    Trial of importing csv's through django

--- cms/school_app/management/commands/uploadexample.py
+++ cms/school_app/management/commands/uploadexample.py
@@ -22,47 +22,63 @@ class Command(BaseCommand):
         parser.add_argument('filename')
 
     def handle(self, *args, **options):
-        try:
-            # Get the school object if it already exists
-            # SchoolID is not globally unique but is unique within a country
-            school = SchoolV2.objects.get(country="Imaginaryland", school_id="TEST001")
-        except SchoolV2.DoesNotExist:
-            # Create the school object if it doesn't exist
-            school = SchoolV2(
-                country="Imaginaryland",
-                school_id="TEST001",
-                school_name="Test School Please Ignore",
-                lat=90,
-                lon=0,
-            )
-            # Write object from memory to the database
-            school.save()
 
-        print(options['filename'])
-        # Create the session under the school
-        session = SchoolV2Session(
-            school=school,
-            data_year=2000,
-            session="Morning",
-            total_enrollment=1,
-            test_score=50,
-        )
-        # Write object from memory to the database
-        session.save()
-
-        import_csv(options['filename'])
-
+        csv_object = import_csv(options['filename'])
     
 def import_csv(filename):
     with open(filename, "r", encoding="utf-8") as fil:
         reader = csv.reader(fil, delimiter=',')
         line_count = 0
+        #initialize a dictionary with column names as keys
+        headers = []
+        headers = next(reader)
+        header_dict = {}
+        for header in headers:
+            header_dict[header] = None
+        print(header_dict)
+        #get country name since every entry will be from the same country's csv
+        for char in range(0, len(filename) - 1):
+            if filename[char] == "/" or filename[char] == "\\":
+                #get the index of the last slash in the path
+                last_slash_idx = char
+        #slice the filename string to get name of file apart from the file type (ex: .csv)
+        country = filename[(last_slash_idx+1):len(filename) - 4]
         for row in reader:
-            if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
-                line_count += 1
-            else:
-                print(f'\t{row[0]}, {row[1]}, {row[2]}.')
-                line_count += 1
+            results = header_dict
+            counter = 0
+            for result in results:
+                results[result] = row[counter]
+                counter += 1
+            line_count += 1
+            print(results)
+            try:
+                # Get the school object if it already exists
+                # SchoolID is not globally unique but is unique within a country
+                school = SchoolV2.objects.get(country=country, school_id=results['SchoolID'])
+            except SchoolV2.DoesNotExist:
+                # Create the school object if it doesn't exist
+                school = SchoolV2(
+                    country=country,
+                    school_id=results['SchoolID'],
+                    school_name=results['SchoolName'],
+                    municipality=results['Municipality'],
+                    lat=results['Latitude'],
+                    lon=results['Longitude'],
+                )
+                # Write object from memory to the database
+                school.save()
+
+            session = SchoolV2Session(
+                school=school,
+                data_year=results['year'],
+                session=results['Session'],
+                total_enrollment=int(float(results['No.StudentsEval'])),
+                test_score=results['total_score'],
+            )
+            # Write object from memory to the database
+            session.save()
+
         print(f'Processed {line_count} lines.')
+        
+    return results
 
