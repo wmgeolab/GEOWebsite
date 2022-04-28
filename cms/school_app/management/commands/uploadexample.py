e3b801893c8b19a8a95a724ad29e9ebf6e162f41
Kerry Wang <kwzeyunwang@gmail.com>
2022-04-28 10:20:58 -0400

    lint

--- cms/school_app/management/commands/uploadexample.py
+++ cms/school_app/management/commands/uploadexample.py
@@ -19,30 +19,30 @@ import csv
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('filename')
+        parser.add_argument("filename")
 
     def handle(self, *args, **options):
+        csv_object = import_csv(options["filename"])
 
-        csv_object = import_csv(options['filename'])
-    
+
 def import_csv(filename):
     with open(filename, "r", encoding="utf-8") as fil:
-        reader = csv.reader(fil, delimiter=',')
+        reader = csv.reader(fil, delimiter=",")
         line_count = 0
-        #initialize a dictionary with column names as keys
+        # initialize a dictionary with column names as keys
         headers = []
         headers = next(reader)
         header_dict = {}
         for header in headers:
             header_dict[header] = None
         print(header_dict)
-        #get country name since every entry will be from the same country's csv
+        # get country name since every entry will be from the same country's csv
         for char in range(0, len(filename) - 1):
             if filename[char] == "/" or filename[char] == "\\":
-                #get the index of the last slash in the path
+                # get the index of the last slash in the path
                 last_slash_idx = char
-        #slice the filename string to get name of file apart from the file type (ex: .csv)
-        country = filename[(last_slash_idx+1):len(filename) - 4]
+        # slice the filename string to get name of file apart from the file type (ex: .csv)
+        country = filename[(last_slash_idx + 1) : len(filename) - 4]
         for row in reader:
             results = header_dict
             counter = 0
@@ -57,31 +57,32 @@ def import_csv(filename):
             try:
                 # Get the school object if it already exists
                 # SchoolID is not globally unique but is unique within a country
-                school = SchoolV2.objects.get(country=country, school_id=results['SchoolID'])
+                school = SchoolV2.objects.get(
+                    country=country, school_id=results["SchoolID"]
+                )
             except SchoolV2.DoesNotExist:
                 # Create the school object if it doesn't exist
                 school = SchoolV2(
                     country=country,
-                    school_id=results['SchoolID'],
-                    school_name=results['SchoolName'],
-                    municipality=results['Municipality'],
-                    lat=results['Latitude'],
-                    lon=results['Longitude'],
+                    school_id=results["SchoolID"],
+                    school_name=results["SchoolName"],
+                    municipality=results["Municipality"],
+                    lat=results["Latitude"],
+                    lon=results["Longitude"],
                 )
                 # Write object from memory to the database
                 school.save()
 
             session = SchoolV2Session(
                 school=school,
-                data_year=results['year'],
-                session=results['Session'],
-                total_enrollment=int(float(results['No.StudentsEval'])),
-                test_score=results['total_score'],
+                data_year=results["year"],
+                session=results["Session"],
+                total_enrollment=int(float(results["No.StudentsEval"])),
+                test_score=results["total_score"],
             )
             # Write object from memory to the database
             session.save()
 
-        print(f'Processed {line_count} lines.')
-        
-    return results
+        print(f"Processed {line_count} lines.")
 
+    return results
