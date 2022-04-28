63ad047803d4c2f062e7c196a5a6f044661381a8
Kerry Wang <kwzeyunwang@gmail.com>
2022-04-28 16:00:45 -0400

    bulk create schools

--- cms/school_app/management/commands/uploadexample.py
+++ cms/school_app/management/commands/uploadexample.py
@@ -6,6 +6,14 @@ from school_app.models import SchoolV2, SchoolV2Session
 # Example management command to add a single record to the database
 # Run with `python manage.py uploadexample <filename>`
 
+# Inserting records one-by-one creates a new transaction for each record, which
+# gets very slow for 1,000+ records.
+# Solution: First, loop through and add every School to a list and insert into the
+# database in a single transaction with buik_create(). The database enforces uniqueness
+# so we can just catch IntegrityError and pass.
+# Then, do a second pass to add each Session. School lookups are fast because they are
+# indexed and cached. Insert with bulk_create().
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -14,8 +22,28 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open(options["filename"], "r", encoding="utf-8") as file:
             reader = csv.DictReader(file)
-            line_count = 0
             country = "Mexico"  # Hardcode for now
+
+            schoolList = [
+                SchoolV2(
+                    country=country,
+                    school_id=row["SchoolID"],
+                    school_name=row["SchoolName"],
+                    municipality=row["Municipality"],
+                    lat=row["Latitude"]
+                    if row["Latitude"] and row["Longitude"]
+                    else None,
+                    lon=row["Longitude"]
+                    if row["Latitude"] and row["Longitude"]
+                    else None,
+                )
+                for row in reader
+            ]
+            SchoolV2.objects.bulk_create(schoolList, ignore_conflicts=True)
+
+            file.seek(0)
+
+            """
             for row in reader:
                 if row["Latitude"] == "" or row["Longitude"] == "":
                     row["Latitude"] = None
@@ -43,3 +71,4 @@ class Command(BaseCommand):
                 if created:
                     line_count += 1
             self.stdout.write(f"Processed {line_count} new lines.")
+            """
