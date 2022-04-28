ab6b69e8136e124717dec8a242c3ee4ce9272fb5
Kerry Wang <kwzeyunwang@gmail.com>
2022-04-28 18:21:02 -0400

    faster insertion algorithm

--- cms/school_app/management/commands/uploadexample.py
+++ cms/school_app/management/commands/uploadexample.py
@@ -7,12 +7,13 @@ from school_app.models import SchoolV2, SchoolV2Session
 # Run with `python manage.py uploadexample <filename>`
 
 # Inserting records one-by-one creates a new transaction for each record, which
-# gets very slow for 1,000+ records.
+# gets very slow for 1,000+ records. It inserted 500 records in ~30 seconds.
+
 # Solution: First, loop through and add every School to a list and insert into the
-# database in a single transaction with buik_create(). The database enforces uniqueness
-# so we can just catch IntegrityError and pass.
+# database in a single transaction with bulk_create(). The database enforces uniqueness
+# so we can just throw everything at it and let it figure it out.
 # Then, do a second pass to add each Session. School lookups are fast because they are
-# indexed and cached. Insert with bulk_create().
+# indexed and cached. Insert with bulk_create(). This inserted 500 records in ~1 second.
 
 
 class Command(BaseCommand):
@@ -41,34 +42,20 @@ class Command(BaseCommand):
             ]
             SchoolV2.objects.bulk_create(schoolList, ignore_conflicts=True)
 
-            file.seek(0)
+            file.seek(0)  # Return to beginning of file
+            next(reader)  # Skip the header row
 
-            """
-            for row in reader:
-                if row["Latitude"] == "" or row["Longitude"] == "":
-                    row["Latitude"] = None
-                    row["Longitude"] = None
-
-                school, _ = SchoolV2.objects.get_or_create(
-                    country=country,
-                    school_id=row["SchoolID"],
-                    defaults={
-                        "school_name": row["SchoolName"],
-                        "municipality": row["Municipality"],
-                        "lat": row["Latitude"],
-                        "lon": row["Longitude"],
-                    },
-                )
-
-                _, created = SchoolV2Session.objects.get_or_create(
-                    school=school,
+            sessionList = [
+                SchoolV2Session(
+                    school=SchoolV2.objects.get(
+                        country=country,
+                        school_id=row["SchoolID"],
+                    ),
                     data_year=row["year"],
                     session=row["Session"],
                     total_enrollment=int(float(row["No.StudentsEval"])),
                     test_score=row["total_score"],
                 )
-
-                if created:
-                    line_count += 1
-            self.stdout.write(f"Processed {line_count} new lines.")
-            """
+                for row in reader
+            ]
+            SchoolV2Session.objects.bulk_create(sessionList, ignore_conflicts=True)
