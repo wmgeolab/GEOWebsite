61bba06f7faa41578783b2d0a37569098d7bf47f
Kerry Wang <kwzeyunwang@gmail.com>
2022-05-05 12:33:07 -0400

    multithreading

--- cms/school_app/management/commands/uploadexample.py
+++ cms/school_app/management/commands/uploadexample.py
@@ -1,5 +1,7 @@
 import csv
+from concurrent.futures import ThreadPoolExecutor, wait
 
+from django.db.models.query import QuerySet
 from django.core.management.base import BaseCommand
 from school_app.models import SchoolV2, SchoolV2Session
 
@@ -18,6 +20,29 @@ from school_app.models import SchoolV2, SchoolV2Session
 # ...until you actually try to insert to the MySQL database, and it bogs down to
 # ~50 records per second. Wtf.
 
+# Implemented multithreaded insert. SQLite hates it (set max_workers=1 for that), but
+# increases MySQL performance to ~430 records per second.
+
+
+def make_session(row: dict, schoolQuery: QuerySet, country: str) -> SchoolV2Session:
+    return SchoolV2Session(
+        school=schoolQuery.get(
+            country=country,
+            school_id=row["school_id"],
+        ),
+        session=row["session"] if "session" in row else "",
+        data_year=row["data_year"] if "data_year" in row and row["data_year"] else None,
+        total_enrollment=int(float(row["total_enrollment"]))
+        if "total_enrollment" in row
+        else None,
+        test_score=row["test_score"]
+        if "test_score" in row and row["test_score"]
+        else None,
+        gender_ratio=row["gender_ratio"]
+        if "gender_ratio" in row and row["gender_ratio"]
+        else None,
+    )
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -28,7 +53,7 @@ class Command(BaseCommand):
         with open(options["filename"], "r", encoding="utf-8") as file:
             reader = csv.DictReader(file)
             country = options["country"]
-
+            self.stdout.write("Creating School objects...")
             schoolList = [
                 SchoolV2(
                     country=country,
@@ -55,34 +80,24 @@ class Command(BaseCommand):
                 )
                 for row in reader
             ]
+            self.stdout.write("Uploading School objects...")
             SchoolV2.objects.bulk_create(schoolList, ignore_conflicts=True)
+
+            schoolQuery = SchoolV2.objects.filter(country=country)
 
             file.seek(0)  # Return to beginning of file
             next(reader)  # Skip the header row
+            self.stdout.write("Creating Session objects...")
+            with ThreadPoolExecutor() as exec:
+                futuresList = [
+                    exec.submit(make_session, row, schoolQuery, country)
+                    for row in reader
+                ]
+                doneFuturesSet, _ = wait(futuresList)
+                sessionList = [future.result() for future in doneFuturesSet]
 
-            sessionList = [
-                SchoolV2Session(
-                    school=SchoolV2.objects.get(
-                        country=country,
-                        school_id=row["school_id"],
-                    ),
-                    session=row["session"] if "session" in row else "",
-                    data_year=row["data_year"]
-                    if "data_year" in row and row["data_year"]
-                    else None,
-                    total_enrollment=int(float(row["total_enrollment"]))
-                    if "total_enrollment" in row
-                    else None,
-                    test_score=row["test_score"]
-                    if "test_score" in row and row["test_score"]
-                    else None,
-                    gender_ratio=row["gender_ratio"]
-                    if "gender_ratio" in row and row["gender_ratio"]
-                    else None,
-                )
-                for row in reader
-            ]
+            self.stdout.write("Uploading Session objects...")
             inserted = SchoolV2Session.objects.bulk_create(
                 sessionList, ignore_conflicts=True
             )
-            self.stdout.write(f"Processed {len(inserted)} records.")
+            self.stdout.write(self.style.SUCCESS(f"Processed {len(inserted)} records."))
